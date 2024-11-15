25f0aa2dc49dda4b48e4bc82517bbea45c81a282
Your Name <berat.bajraktari11@gmail.com>
2024-11-15 09:37:56 +0100

    Commit

---
+++ Berati.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BeratiBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeratiBook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        
+    }
+}
